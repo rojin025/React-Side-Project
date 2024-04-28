9cc2d75dcbc918c9f7659d354f89a028d6c03b14
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-04-28 18:00:31 -0500

    Redux Intro. V15.0.4 - Implementing Loan Request. Happy Coding :)

--- ultimate-react-course-classwork/15-redux-intro/redux-intro/src/index.tsx
+++ ultimate-react-course-classwork/15-redux-intro/redux-intro/src/index.tsx
@@ -1,26 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 
 import store from "./store";
-
-store.dispatch({
-  type: "account/deposit",
-  payload: {
-    balance: 250,
-    loan: 0,
-    loanPurpose: "",
-  },
-});
-
-console.log(store.getState());
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
