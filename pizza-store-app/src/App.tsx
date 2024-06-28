449ccb2322fec01d069c2440e8308a987cf57047
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-06-27 22:29:27 -0500

    Adding app Layout router. Happy coding :)

--- pizza-store-app/src/App.tsx
+++ pizza-store-app/src/App.tsx
@@ -1,9 +1,42 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui-component/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui-component/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <div>Hello World!!!</div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- pizza-store-app/src/ui-component/Home.jsx
+++ pizza-store-app/src/ui-component/Home.jsx
@@ -1,4 +1,4 @@
-function Home() {
+export default function Home() {
   return (
     <div>
       <h1>
@@ -10,4 +10,4 @@ function Home() {
   );
 }
 
-export default Home;
+// export default Home;
