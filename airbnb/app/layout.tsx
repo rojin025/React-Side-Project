67f6b81320d0a448708fd85c9da663b955a882f5
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-11-11 12:00:41 -0600

    Adding Loading, Font Optimizing with google font, Improved Nav & layout. Happy coding :)

--- airbnb/app/layout.tsx
+++ airbnb/app/layout.tsx
@@ -1,12 +1,18 @@
-import Logo from "@/app/_components/Logo";
-import Navigation from "@/app/_components/Navigation";
+import Header from "@/app/_components/Header";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: {
     template: "%s | AirBnB",
-    default: "AirBnB",
+    default: "AirBnB | For you",
   },
   description: "Where home are rented! This is for SEO.",
 };
@@ -18,13 +24,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
+      <body
+        className={`${josefin.className} flex flex-col relative bg-primary-950 text-primary-100  antialiased min-h-screen`}
+      >
+        <Header />
 
-        <main>{children}</main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main>{children}</main>
+        </div>
 
         <footer>Copyright by the AirBnB</footer>
       </body>
