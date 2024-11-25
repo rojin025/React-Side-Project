6b37c7628e8a2e516d501124dedf6b07c01d4e67
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-11-24 19:20:23 -0600

    Adding Auth.js

--- airbnb/app/_components/Navigation.tsx
+++ airbnb/app/_components/Navigation.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
+import { auth } from '@/app/_lib/auth';
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+
   return (
     <nav className='z-10 text-xl'>
       <ul className='flex items-center gap-16'>
