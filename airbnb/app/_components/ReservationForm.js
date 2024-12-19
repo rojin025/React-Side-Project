b8ea1a020cc1fff4b0e4c291143fa1383d77d640
Rojin Bijukchhe <ro_jin25@icloud.com>
2024-12-18 18:14:23 -0600

    COMP | Adding pay now button.

--- airbnb/app/_components/ReservationForm.js
+++ airbnb/app/_components/ReservationForm.js
@@ -81,9 +81,14 @@ function ReservationForm({ cabin, user }) {
               Start by selecting dates
             </p>
           ) : (
-            <button className='bg-accent-500 px-8 py-4 font-semibold text-primary-800 transition-all hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'>
-              Reserve now
-            </button>
+            <>
+              <button className='bg-accent-500 px-8 py-4 font-semibold text-primary-800 transition-all hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'>
+                Pay now
+              </button>
+              <button className='bg-accent-500 px-8 py-4 font-semibold text-primary-800 transition-all hover:bg-accent-600 disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300'>
+                Reserve now
+              </button>
+            </>
           )}
         </div>
       </form>
